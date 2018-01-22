74fd53f811f7e168f6d3828f045968553b410827
Caleb Fahlgren <cfahlgren1@gmail.com>
2018-01-22 16:35:17 -0500

    added support for multiple command line arguments for file inputs. For example, you would be able to find the hash of multiple files by adding more commandline parameters instead of just one

--- sha1.py
+++ sha1.py
@@ -3,15 +3,19 @@
 from __future__ import print_function
 import struct
 import io
+from termcolor import colored
+
 
 try:
     range = xrange
 except NameError:
     pass
 
+
 def _left_rotate(n, b):
     """Left rotate a 32-bit integer n by b bits."""
     return ((n << b) | (n >> (32 - b))) & 0xffffffff
+
 
 def _process_chunk(chunk, h0, h1, h2, h3, h4):
     """Process a chunk of data and return the new digest variables."""
@@ -21,19 +25,19 @@ def _process_chunk(chunk, h0, h1, h2, h3, h4):
 
     # Break chunk into sixteen 4-byte big-endian words w[i]
     for i in range(16):
-        w[i] = struct.unpack(b'>I', chunk[i*4:i*4 + 4])[0]
+        w[i] = struct.unpack(b'>I', chunk[i * 4:i * 4 + 4])[0]
 
     # Extend the sixteen 4-byte words into eighty 4-byte words
     for i in range(16, 80):
-        w[i] = _left_rotate(w[i-3] ^ w[i-8] ^ w[i-14] ^ w[i-16], 1)
-    
+        w[i] = _left_rotate(w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16], 1)
+
     # Initialize hash value for this chunk
     a = h0
     b = h1
     c = h2
     d = h3
     e = h4
-    
+
     for i in range(80):
         if 0 <= i <= 19:
             # Use alternative 1 for f from FIPS PB 180-1 to avoid bitwise not
@@ -43,23 +47,24 @@ def _process_chunk(chunk, h0, h1, h2, h3, h4):
             f = b ^ c ^ d
             k = 0x6ED9EBA1
         elif 40 <= i <= 59:
-            f = (b & c) | (b & d) | (c & d) 
+            f = (b & c) | (b & d) | (c & d)
             k = 0x8F1BBCDC
         elif 60 <= i <= 79:
             f = b ^ c ^ d
             k = 0xCA62C1D6
-    
-        a, b, c, d, e = ((_left_rotate(a, 5) + f + e + k + w[i]) & 0xffffffff, 
-                        a, _left_rotate(b, 30), c, d)
-    
+
+        a, b, c, d, e = ((_left_rotate(a, 5) + f + e + k + w[i]) & 0xffffffff,
+                         a, _left_rotate(b, 30), c, d)
+
     # Add this chunk's hash to result so far
     h0 = (h0 + a) & 0xffffffff
-    h1 = (h1 + b) & 0xffffffff 
+    h1 = (h1 + b) & 0xffffffff
     h2 = (h2 + c) & 0xffffffff
     h3 = (h3 + d) & 0xffffffff
     h4 = (h4 + e) & 0xffffffff
 
     return h0, h1, h2, h3, h4
+
 
 class Sha1Hash(object):
     """A class that mimics that hashlib api and implements the SHA-1 algorithm."""
@@ -88,7 +93,7 @@ class Sha1Hash(object):
         """Update the current digest.
 
         This may be called repeatedly, even after calling digest or hexdigest.
-        
+
         Arguments:
             arg: bytes, bytearray, or BytesIO object to read from.
         """
@@ -107,7 +112,6 @@ class Sha1Hash(object):
         self._unprocessed = chunk
         return self
 
-
     def digest(self):
         """Produce the final hash value (big-endian) as a bytes object"""
         return b''.join(struct.pack(b'>I', h) for h in self._produce_digest())
@@ -124,11 +128,11 @@ class Sha1Hash(object):
 
         # append the bit '1' to the message
         message += b'\x80'
-        
+
         # append 0 <= k < 512 bits '0', so that the resulting message length (in bytes)
         # is congruent to 56 (mod 64)
         message += b'\x00' * ((56 - (message_byte_length + 1) % 64) % 64)
-        
+
         # append length of message (before pre-processing), in bits, as 64-bit big-endian integer
         message_bit_length = message_byte_length * 8
         message += struct.pack(b'>Q', message_bit_length)
@@ -153,23 +157,23 @@ def sha1(data):
         A hex SHA-1 digest of the input message.
     """
     return Sha1Hash().update(data).hexdigest()
-    
-    
+
+
 if __name__ == '__main__':
     # Imports required for command line parsing. No need for these elsewhere
     import argparse
-    import sys 
+    import sys
     import os
 
-    # Parse the incoming arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input', nargs='?',
-             help='input file or message to hash')
-    args = parser.parse_args()
+#    # Parse the incoming arguments
+#    parser = argparse.ArgumentParser()
+#    parser.add_argument('input', nargs='?',
+#                        help='input file or message to hash')
+#    args = parser.parse_args()
 
     data = None
 
-    if args.input is None:
+    if len(sys.argv[1:]) == 0:
         # No argument given, assume message comes from standard input
         try:
             # sys.stdin is opened in text mode, which can change line endings,
@@ -179,15 +183,28 @@ if __name__ == '__main__':
         except AttributeError:
             # Linux ans OSX both use \n line endings, so only windows is a
             # problem.
-            if sys.platform == "win32": 
+            if sys.platform == "win32":
                 import msvcrt
-                msvcrt.setmode(sys.stdin.fileno(), os.O_BINARY) 
+
+                msvcrt.setmode(sys.stdin.fileno(), os.O_BINARY)
             data = sys.stdin
-    elif os.path.isfile(args.input):
-        # An argument is given and it's a valid file. Read it
-        data = open(args.input, 'rb')
     else:
-        data = args.input
-    
-    # Show the final digest
-    print('sha1-digest:', sha1(data))
+        for argument in sys.argv[1:]:
+            if os.path.isfile(argument):
+                # An argument is given and it's a valid file. Read it
+                data = open(argument, 'rb')
+            else:
+                data = argument
+
+            # Show the final digest
+            try:
+                print('sha1-digest:', sha1(data))
+            except Exception:
+                #Print colored error message
+                print (colored ("Error, could not find ", "red") + colored(argument, "yellow") + colored(" file.", "red"))
+
+
+
+
+
+
